f94b0d8a152632b487959a27ac88d6f8fb17c70b
John Wästerlund <grunka@gmail.com>
2024-03-26 12:34:23 +0100

    Completing the javadoc

--- src/main/java/com/grunka/json/ParsedJson.java
+++ src/main/java/com/grunka/json/ParsedJson.java
@@ -2,5 +2,10 @@ package com.grunka.json;
 
 import com.grunka.json.type.JsonValue;
 
+/**
+ * Object representing potentially partially parsed JSON
+ * @param jsonValue the JSON successfully parsed
+ * @param remainder the remaining string of non-parsed content
+ */
 public record ParsedJson(JsonValue jsonValue, String remainder) {
 }
--- src/main/java/com/grunka/json/type/JsonObject.java
+++ src/main/java/com/grunka/json/type/JsonObject.java
@@ -66,6 +66,12 @@ public class JsonObject implements JsonValue, Map<String, JsonValue> {
         return values.get(key);
     }
 
+    /**
+     * Get a field from the object and directly converts it to an JsonObject
+     * @param key the key in the object
+     * @return the value represented by the key as an JsonObject
+     * @throws IllegalArgumentException if the value is not a JsonObject
+     */
     public JsonObject getObject(String key) {
         JsonValue jsonValue = values.get(key);
         if (jsonValue == null || jsonValue == JsonNull.NULL) {
@@ -77,6 +83,12 @@ public class JsonObject implements JsonValue, Map<String, JsonValue> {
         return jsonValue.asObject();
     }
 
+    /**
+     * Get a field from the object and directly converts it to an JsonArray
+     * @param key the key in the object
+     * @return the value represented by the key as a JsonArray
+     * @throws IllegalArgumentException if the value is not a JsonArray
+     */
     public JsonArray getArray(String key) {
         JsonValue jsonValue = values.get(key);
         if (jsonValue == null || jsonValue == JsonNull.NULL) {
@@ -88,6 +100,12 @@ public class JsonObject implements JsonValue, Map<String, JsonValue> {
         return jsonValue.asArray();
     }
 
+    /**
+     * Get a field from the object and directly converts it to an JsonString and gets the string value from it
+     * @param key the key in the object
+     * @return the value represented by the key as a String
+     * @throws IllegalArgumentException if the value is not a JsonString
+     */
     public String getString(String key) {
         JsonValue jsonValue = values.get(key);
         if (jsonValue == null || jsonValue == JsonNull.NULL) {
@@ -99,6 +117,12 @@ public class JsonObject implements JsonValue, Map<String, JsonValue> {
         return jsonValue.asString().getString();
     }
 
+    /**
+     * Get a field from the object and directly converts it to an JsonNumber and gets the BigDecimal value from it
+     * @param key the key in the object
+     * @return the value represented by the key as a BigDecimal
+     * @throws IllegalArgumentException if the value is not a JsonNumber
+     */
     public BigDecimal getBigDecimal(String key) {
         JsonValue jsonValue = values.get(key);
         if (jsonValue == null || jsonValue == JsonNull.NULL) {
@@ -110,6 +134,12 @@ public class JsonObject implements JsonValue, Map<String, JsonValue> {
         return jsonValue.asNumber().getBigDecimal();
     }
 
+    /**
+     * Get a field from the object and directly converts it to an JsonBoolean and gets the Boolean value from it
+     * @param key the key in the object
+     * @return the value represented by the key as a Boolean
+     * @throws IllegalArgumentException if the value is not a JsonBoolean
+     */
     public Boolean getBoolean(String key) {
         JsonValue jsonValue = values.get(key);
         if (jsonValue == null || jsonValue == JsonNull.NULL) {
